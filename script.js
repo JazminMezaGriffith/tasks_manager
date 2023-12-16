6f70719ffff12a7a7301d5a61e62d55f1fbe38ea
LizandroGBritto <80988976+LizandroGBritto@users.noreply.github.com>
2023-12-16 10:09:54 -0300

    Fix the issues with the delete button

--- script.js
+++ script.js
@@ -1,66 +1,170 @@
-class Task {
-    constructor(id, categories, taskTitle, description, dateFrom, dateTo) {
-        this.id = id;
-        this.categories = categories;
-        this.taskTitle = taskTitle;
-        this.description = description;
-        this.dateFrom = dateFrom;
-        this.dateTo = dateTo;
-    }
+let taskList = [];
+
+const objTask = {
+    id: 0,
+    categories: '',
+    taskTitle: '',
+    description: '',
+    day:0,
+    month:0,
+    year:0,
+    day2:0,
+    month2:0,
+    year2:0,
 }
-class UI {
-    addTask(task){
-        const taskList = document.getElementById('tasks');
-            const element = document.createElement('div');
-            element.innerHTML = `
-            <div class="card">
-            <h3>${task.taskTitle}</h3>
-            <p><strong>Category: ${task.categories} </strong></p>
-            <p><strong> Status: </strong> </p>
-            <p>${task.description}</p>
-            <p>Date: ${task.dateFrom} to ${task.dateFrom}</p>
-            <div class="button-container">
-                <button id="btn_edit">Edit <img src="images/pencil-square.svg" alt="Edit"></button>
-                <button id="btn_delete" name="delete">Delete <img src="images/trash.svg" alt="Delete"></button>
-            </div>
-        </div>
-            `;
-            taskList.appendChild(element);
-            formTask.reset();
-      
-    }
-    deleteTask(element){
-        if (element.name === 'delete'){
-            element.parentElement.parentElement.parentElement.parentElement.remove();
-        }
-    }
+let editStatus = false;
 
+const formTask = document.querySelector('#formTask');
+const categories = document.querySelector('#categories');
+const taskTitle = document.querySelector('#taskTitle');
+const taskDescription = document.querySelector('#taskDescription');
+const day = document.querySelector('#day');
+const month = document.querySelector('#month');
+const year = document.querySelector('#year');
+const day2 = document.querySelector('#day2');
+const month2 = document.querySelector('#month2');
+const year2 = document.querySelector('#year2');
+const btnSave = document.querySelector('#btnSave');
 
-    showMessage(){
+formTask.addEventListener('submit', validateForm);
 
-    }
-}
-
-//DOM Events
-document.getElementById('formTask').addEventListener('submit', function(e){
-    const id = Date.now();
-    const categories = document.getElementById('categories').value;
-    const taskTitle = document.getElementById('taskTitle').value;
-    const description = document.getElementById('taskDescription').value;
-    const dateFrom = document.getElementById('dateFrom').value;
-    const dateTo = document.getElementById('dateTo').value;
-    const task = new Task(id, categories, taskTitle, description, dateFrom, dateTo);
-
-    const ui = new UI();
-    ui.addTask(task);
-
-
+function validateForm(e){
     e.preventDefault();
+    if(categories.value ==='' || taskTitle.value == '' || taskDescription.value == '' || day.value == '' || month.value == '' || year.value == '' || day2.value == '' || month2.value == '' || year2.value == ''){
+        alert('Please fill all the fields');
+        return;
+    }
+    if(editStatus){
+        updateTask();
+        editStatus = false;
+    }else{
+        objTask.id = objTask.id + 1;
+        objTask.categories = categories.value;
+        objTask.taskTitle = taskTitle.value;
+        objTask.description = taskDescription.value;
+        objTask.day = day.value;
+        objTask.month = month.value;
+        objTask.year = year.value;
+        objTask.day2 = day2.value;
+        objTask.month2 = month2.value;
+        objTask.year2 = year2.value;
+        addTask();
 
-    document.getElementById('tasks').addEventListener('click', function(e){
-        const ui = new UI();
-        ui.deleteTask(e.target);
+    }
+    formTask.reset();
+}
+function addTask(){
+    taskList.push({...objTask});
+    showTasks();
+    formTask.reset();
+}
+
+function  showTasks(){
+    cleanHtml();
+
+    const taskContainer = document.querySelector('#tasks');
+    taskList.forEach(task => {
+        const {id, categories, taskTitle, description, day, month, year, day2, month2, year2} = task;
+        const div = document.createElement('div');
+        div.className = 'card';
+      
+        div.innerHTML = `
+        <h3>${id} ${taskTitle}</h3>
+        <p><strong>Category: ${categories} </strong></p>
+        <p><strong> Status: </strong> </p>
+        <p>${description}</p>
+        <p>Date: ${day} / ${month}/ ${year} to ${day2} / ${month2}/ ${year2}</p>
+        <div class="button-container">
+            <button id="btn_edit"  name="edit">Edit <img src="images/pencil-square.svg" alt="Edit"></button>
+            <button id="btn_delete" name="delete">Delete <img src="images/trash.svg" alt="Delete"></button>
+        </div>
+        `;
+        taskContainer.appendChild(div);
+        div.dataset.id = id;
+
+        const btnDelete = document.getElementById('btn_delete');
+        btnDelete.onclick = () => deleteTask(task);
+        const btnEdit = document.getElementById('btn_edit');
+        btnEdit.onclick = () => loadTask(task);
         
     });
+}
+
+document.getElementById('tasks').addEventListener('click', function (e) {
+    const target = e.target;
+    if (target.name === 'delete') {
+        deleteTask(target.parentElement.parentElement);
+    } else if (target.name === 'edit') {
+        const taskId = target.parentElement.parentElement.dataset.id;
+        const task = taskList.find(task => task.id === parseInt(taskId));
+        loadTask(task);
+    }
 });
+
+function deleteTask(taskElement) {
+    const taskId = taskElement.dataset.id;
+    taskList = taskList.filter(task => task.id !== parseInt(taskId));
+    taskElement.remove();
+}
+
+function loadTask(task) {
+    const { id, categories, taskTitle, description, dateFrom, dateTo } = task;
+    categories.value = categories;
+    taskTitle.value = taskTitle;
+    taskDescription.value = description;  // Aquí debería ser taskDescription en lugar de description
+    day.value = day;
+    month.value = month;
+    year.value = year;
+    day2.value = day2;
+    month2.value = month2;
+    year2.value = year2;
+    objTask.id = id;
+    btnSave.textContent = 'Update';
+    editStatus = true;
+}
+function cleanHtml(){
+        const taskContainer = document.querySelector('#tasks');
+        while(taskContainer.firstChild){
+            taskContainer.removeChild(taskContainer.firstChild);
+        }
+}
+function editTask() {
+    objTask.categories = categories.value;
+    objTask.taskTitle = taskTitle.value;
+    objTask.description = taskDescription.value;
+    objTask.day = day.value;
+    objTask.month = month.value;
+    objTask.year = year.value;
+    objTask.day2 = day2.value;
+    objTask.month2 = month2.value;
+    objTask.year2 = year2.value;
+
+    const index = taskList.findIndex(task => task.id === objTask.id);
+    if (index !== -1) {
+        taskList[index] = { ...objTask };
+        updateTaskInUI(objTask);
+        formTask.reset();
+        btnSave.textContent = 'Save';
+        editStatus = false;
+    }
+}
+function updateTaskInUI(updatedTask) {
+    const taskContainer = document.querySelector('#tasks');
+    const existingTaskElement = taskContainer.querySelector(`div[data-id="${updatedTask.id}"]`);
+    if (existingTaskElement) {
+        existingTaskElement.innerHTML = `
+            <h3>${updatedTask.id} ${updatedTask.taskTitle}</h3>
+            <p><strong>Category: ${updatedTask.categories} </strong></p>
+            <p><strong> Status: </strong> </p>
+            <p>${updatedTask.description}</p>
+            <p>Date: ${updatedTask.dateFrom} to ${updatedTask.dateTo}</p>
+            <div class="button-container">
+                <button id="btn_edit"  name="edit">Edit <img src="images/pencil-square.svg" alt="Edit"></button>
+                <button id="btn_delete" name="delete">Delete <img src="images/trash.svg" alt="Delete"></button>
+            </div>
+        `;
+    }
+}
+
+
 
